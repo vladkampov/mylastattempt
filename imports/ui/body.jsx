001e457e113ef414cfe4c3bca5160e2a55554c9f
vladkampov <v@kampov.com>
2016-06-24 18:55:28 +0300

    first commit

---
+++ imports/ui/body.jsx
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+import './social/social.html'
+import './header/header.html'
+import './tour/tour.html'
+import './songs/songs.html'
+import './video/video.html'
+import './contacts/contacts.html'
+import './footer/footer.html'
