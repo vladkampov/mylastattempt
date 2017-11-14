9f10756aa7e16bc925e05a69dbe88beb7a012bde
vladkampov <vlad@geeny.io>
2017-11-14 22:32:46 +0200

    Album pre-release

--- client/main.jsx
+++ client/main.jsx
@@ -3,7 +3,7 @@ import '../imports/ui/body.jsx';
 $(() => {
     $('a[href^="#"]').click((e)=> {
         e.preventDefault()
-        destination = $(e.target).parent().attr('href')
-        $('body').animate({scrollTop: $(destination).offset().top}, 500)
+        destination = $(e.target).attr('href');
+        $('body, html').animate({scrollTop: $(destination).offset().top}, 500, 'swing')
     })
 });
